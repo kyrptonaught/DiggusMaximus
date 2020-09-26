17ba02eefac106fcb768c8eef6f0390f32882487
kyrptonaught <anthonybuttillo10@gmail.com>
2020-09-26 16:57:25 -0400

    Change mixin to cancel durability loss when excavating, fixed shears not being canceled correctly

--- src/main/java/net/kyrptonaught/diggusmaximus/mixin/MixinPlayerEntity.java
+++ src/main/java/net/kyrptonaught/diggusmaximus/mixin/MixinPlayerEntity.java
@@ -1,20 +1,15 @@
 package net.kyrptonaught.diggusmaximus.mixin;
 
 import net.kyrptonaught.diggusmaximus.DiggingPlayerEntity;
-import net.kyrptonaught.diggusmaximus.DiggusMaximusMod;
 import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public class MixinPlayerEntity implements DiggingPlayerEntity {
-    @Shadow protected HungerManager hungerManager;
+    @Shadow
+    protected HungerManager hungerManager;
     private boolean isExcavating = false;
 
     @Override
