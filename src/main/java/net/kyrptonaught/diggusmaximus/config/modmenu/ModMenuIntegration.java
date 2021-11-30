a8bc3290211a8c01a8f068c6eebe710825657217
kyrptonaught <anthonybuttillo10@gmail.com>
2021-11-30 16:15:07 -0500

    Port to 1.18. Note this port features limited mod menu config editing support. Some features are missing.

--- src/main/java/net/kyrptonaught/diggusmaximus/config/modmenu/ModMenuIntegration.java
+++ src/main/java/net/kyrptonaught/diggusmaximus/config/modmenu/ModMenuIntegration.java
@@ -2,10 +2,6 @@ package net.kyrptonaught.diggusmaximus.config.modmenu;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
-import me.shedaniel.clothconfig2.api.ConfigCategory;
-import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
-import me.shedaniel.clothconfig2.gui.entries.StringListListEntry;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.kyrptonaught.diggusmaximus.DiggusMaximusClientMod;
@@ -16,11 +12,15 @@ import net.kyrptonaught.diggusmaximus.config.Blacklist;
 import net.kyrptonaught.diggusmaximus.config.BlockCategory;
 import net.kyrptonaught.diggusmaximus.config.ConfigOptions;
 import net.kyrptonaught.diggusmaximus.config.ExcavatingShapes;
-import net.minecraft.client.util.InputUtil;
+import net.kyrptonaught.kyrptconfig.config.screen.ConfigScreen;
+import net.kyrptonaught.kyrptconfig.config.screen.ConfigSection;
+import net.kyrptonaught.kyrptconfig.config.screen.items.BooleanItem;
+import net.kyrptonaught.kyrptconfig.config.screen.items.EnumItem;
+import net.kyrptonaught.kyrptconfig.config.screen.items.KeybindItem;
+import net.kyrptonaught.kyrptconfig.config.screen.items.number.FloatItem;
+import net.kyrptonaught.kyrptconfig.config.screen.items.number.IntegerItem;
 import net.minecraft.text.TranslatableText;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 
 @Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi {
@@ -36,8 +36,8 @@ public class ModMenuIntegration implements ModMenuApi {
         return (screen) -> {
             ConfigOptions options = DiggusMaximusMod.getOptions();
             ExcavatingShapes shapes = DiggusMaximusMod.getExcavatingShapes();
-            ConfigBuilder builder = ConfigBuilder.create().setParentScreen(screen).setTitle(new TranslatableText("Diggus Maximus Config"));
-            builder.setSavingRunnable(() -> {
+            ConfigScreen configScreen = new ConfigScreen(screen, new TranslatableText("Diggus Maximus Config"));
+            configScreen.setSavingEvent(() -> {
                 DiggusMaximusMod.configManager.save();
                 DiggusMaximusClientMod.activationKey.setRaw(options.keybinding);
                 DiggusMaximusClientMod.shapeKey.setRaw(shapes.shapeKey);
@@ -46,51 +46,55 @@ public class ModMenuIntegration implements ModMenuApi {
                 DiggusMaximusMod.getBlackList().generateLookup();
                 ExcavateHelper.resetMaximums();
             });
-            ConfigEntryBuilder entryBuilder = ConfigEntryBuilder.create();
 
-            ConfigCategory category = builder.getOrCreateCategory(new TranslatableText("key.diggusmaximus.config.category"));
-            category.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.enabled"), options.enabled).setSaveConsumer(val -> options.enabled = val).setDefaultValue(true).build());
-            category.addEntry(entryBuilder.startKeyCodeField(new TranslatableText("key.diggusmaximus.config.hotkey"), DiggusMaximusClientMod.activationKey.getKeybinding().orElse(InputUtil.UNKNOWN_KEY)).setSaveConsumer(key -> options.keybinding = key.getTranslationKey()).build());
-            category.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.invertactivation"), options.invertActivation).setSaveConsumer(val -> options.invertActivation = val).setDefaultValue(false).build());
-            category.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.sneaktoexcavate"), options.sneakToExcavate).setSaveConsumer(val -> options.sneakToExcavate = val).setDefaultValue(false).build());
+            ConfigSection mainSection = new ConfigSection(configScreen, new TranslatableText("key.diggusmaximus.config.category"));
 
-            category.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.minediag"), options.mineDiag).setSaveConsumer(val -> options.mineDiag = val).setDefaultValue(true).build());
-            category.addEntry(entryBuilder.startIntField(new TranslatableText("key.diggusmaximus.config.maxmine"), options.maxMinedBlocks).setSaveConsumer(val -> options.maxMinedBlocks = val).setDefaultValue(40).setMin(1).setMax(2048).build());
-            category.addEntry(entryBuilder.startIntField(new TranslatableText("key.diggusmaximus.config.maxdistance"), options.maxMineDistance).setSaveConsumer(val -> options.maxMineDistance = val).setDefaultValue(10).setMin(1).setMax(128).build());
+            mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.enabled"), options.enabled, true).setSaveConsumer(val -> options.enabled = val));
+            mainSection.addConfigItem(new KeybindItem(new TranslatableText("key.diggusmaximus.config.hotkey"), options.keybinding, "key.keyboard.grave.accent").setSaveConsumer(val -> options.keybinding = val));
+            mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.invertactivation"), options.invertActivation, false).setSaveConsumer(val -> options.invertActivation = val));
+            mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.sneaktoexcavate"), options.sneakToExcavate, false).setSaveConsumer(val -> options.sneakToExcavate = val));
 
-            category.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.autopickup"), options.autoPickup).setSaveConsumer(val -> options.autoPickup = val).setDefaultValue(true).build());
-            category.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.requirestool"), options.requiresTool).setSaveConsumer(val -> options.requiresTool = val).setDefaultValue(false).build());
-            category.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.toolduribility"), options.toolDurability).setSaveConsumer(val -> options.toolDurability = val).setDefaultValue(true).build());
-            category.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.stopontoolbreak"), options.stopOnToolBreak).setSaveConsumer(val -> options.stopOnToolBreak = val).setDefaultValue(true).build());
-            category.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.dontbreaktool"), options.dontBreakTool).setSaveConsumer(val -> options.dontBreakTool = val).setDefaultValue(true).build());
-            category.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.playerexhaustion"), options.playerExhaustion).setSaveConsumer(val -> options.playerExhaustion = val).setDefaultValue(true).build());
-            category.addEntry(entryBuilder.startFloatField(new TranslatableText("key.diggusmaximus.config.exhaustionmultiplier"), options.exhaustionMultiplier).setSaveConsumer(val -> options.exhaustionMultiplier = val).setDefaultValue(1.0f).build());
+            mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.minediag"), options.mineDiag, true).setSaveConsumer(val -> options.mineDiag = val));
+            mainSection.addConfigItem(new IntegerItem(new TranslatableText("key.diggusmaximus.config.maxmine"), options.maxMinedBlocks, 40).setMinMax(1,2048).setSaveConsumer(val -> options.maxMinedBlocks = (int) val));
+            mainSection.addConfigItem(new IntegerItem(new TranslatableText("key.diggusmaximus.config.maxdistance"), options.maxMineDistance, 10).setMinMax(1,128).setSaveConsumer(val -> options.maxMineDistance = (int)val));
 
-            category.addEntry(entryBuilder.startStrList(new TranslatableText("key.diggusmaximus.config.toollist"), new ArrayList<>(options.tools)).setSaveConsumer(val -> options.tools = new HashSet<>(val)).setDefaultValue(new ArrayList<>())
-                    .setCreateNewInstance(baseListEntry -> new StringListListEntry.StringListCell("minecraft:)", baseListEntry)).build());
+            mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.minediag"), options.mineDiag, true).setSaveConsumer(val -> options.mineDiag = val));
+
+
+           mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.autopickup"), options.autoPickup,true).setSaveConsumer(val -> options.autoPickup = val));
+           mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.requirestool"), options.requiresTool,false).setSaveConsumer(val -> options.requiresTool = val));
+           mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.toolduribility"), options.toolDurability,true).setSaveConsumer(val -> options.toolDurability = val));
+           mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.stopontoolbreak"), options.stopOnToolBreak,true).setSaveConsumer(val -> options.stopOnToolBreak = val));
+           mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.dontbreaktool"), options.dontBreakTool,true).setSaveConsumer(val -> options.dontBreakTool = val));
+           mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.playerexhaustion"), options.playerExhaustion,true).setSaveConsumer(val -> options.playerExhaustion = val));
+           mainSection.addConfigItem(new FloatItem(new TranslatableText("key.diggusmaximus.config.exhaustionmultiplier"), options.exhaustionMultiplier,1.0f).setSaveConsumer(val -> options.exhaustionMultiplier = val));
+
+            //category.addEntry(entryBuilder.startStrList(new TranslatableText("key.diggusmaximus.config.toollist"), new ArrayList<>(options.tools)).setSaveConsumer(val -> options.tools = new HashSet<>(val)).setDefaultValue(new ArrayList<>())
+            //        .setCreateNewInstance(baseListEntry -> new StringListListEntry.StringListCell("minecraft:)", baseListEntry)).build());
 
             Blacklist blacklist = DiggusMaximusMod.getBlackList();
-            ConfigCategory blacklistCat = builder.getOrCreateCategory(new TranslatableText("key.diggusmaximus.config.blacklistcat"));
-            blacklistCat.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.invertlist"), blacklist.isWhitelist).setSaveConsumer(val -> blacklist.isWhitelist = val).setDefaultValue(false).build());
+            ConfigSection blacklistSection = new ConfigSection(configScreen, new TranslatableText("key.diggusmaximus.config.blacklistcat"));
+            blacklistSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.invertlist"), blacklist.isWhitelist, false).setSaveConsumer(val -> blacklist.isWhitelist = val));
 
-            blacklistCat.addEntry(entryBuilder.startStrList(new TranslatableText("key.diggusmaximus.config.blacklist"), new ArrayList<>(blacklist.blacklistedBlocks)).setSaveConsumer(val -> blacklist.blacklistedBlocks = new HashSet<>(val)).setDefaultValue(new ArrayList<>())
-                    .setCreateNewInstance(baseListEntry -> new StringListListEntry.StringListCell("minecraft:", baseListEntry)).build());
+            //blacklistCat.addEntry(entryBuilder.startStrList(new TranslatableText("key.diggusmaximus.config.blacklist"), new ArrayList<>(blacklist.blacklistedBlocks)).setSaveConsumer(val -> blacklist.blacklistedBlocks = new HashSet<>(val)).setDefaultValue(new ArrayList<>())
+            //        .setCreateNewInstance(baseListEntry -> new StringListListEntry.StringListCell("minecraft:", baseListEntry)).build());
 
             BlockCategory grouping = DiggusMaximusMod.getGrouping();
-            ConfigCategory groupcat = builder.getOrCreateCategory(new TranslatableText("key.diggusmaximus.config.groupcat"));
-            groupcat.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.taggrouping"), grouping.tagGrouping).setSaveConsumer(val -> grouping.tagGrouping = val).setDefaultValue(false).build());
-            groupcat.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.customgrouping"), grouping.customGrouping).setSaveConsumer(val -> grouping.customGrouping = val).setDefaultValue(false).build());
-            groupcat.addEntry(entryBuilder.startStrList(new TranslatableText("key.diggusmaximus.config.grouplist"), grouping.groups).setSaveConsumer(val -> grouping.groups = val).setDefaultValue(new ArrayList<>())
-                    .setCreateNewInstance(baseListEntry -> new StringListListEntry.StringListCell("minecraft:", baseListEntry)).build());
+            ConfigSection groupSection = new ConfigSection(configScreen, new TranslatableText("key.diggusmaximus.config.groupcat"));
+            groupSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.taggrouping"), grouping.tagGrouping,false).setSaveConsumer(val -> grouping.tagGrouping = val));
+            groupSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.customgrouping"), grouping.customGrouping, false).setSaveConsumer(val -> grouping.customGrouping = val));
+            //groupcat.addEntry(entryBuilder.startStrList(new TranslatableText("key.diggusmaximus.config.grouplist"), grouping.groups).setSaveConsumer(val -> grouping.groups = val).setDefaultValue(new ArrayList<>())
+            //        .setCreateNewInstance(baseListEntry -> new StringListListEntry.StringListCell("minecraft:", baseListEntry)).build());
 
-            ConfigCategory shapeCat = builder.getOrCreateCategory(new TranslatableText("key.diggusmaximus.config.shapecat"));
-            shapeCat.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.enableshapes"), shapes.enableShapes).setSaveConsumer(val -> shapes.enableShapes = val).setDefaultValue(false).build());
-            shapeCat.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("key.diggusmaximus.config.includedifblock"), shapes.includeDifBlocks).setSaveConsumer(val -> shapes.includeDifBlocks = val).setDefaultValue(false).build());
-            shapeCat.addEntry(entryBuilder.startKeyCodeField(new TranslatableText("key.diggusmaximus.config.shapekey"), DiggusMaximusClientMod.shapeKey.getKeybinding().orElse(InputUtil.UNKNOWN_KEY)).setSaveConsumer(key -> shapes.shapeKey = key.getTranslationKey()).setDefaultValue(InputUtil.UNKNOWN_KEY).build());
-            shapeCat.addEntry(entryBuilder.startKeyCodeField(new TranslatableText("key.diggusmaximus.config.cyclekey"), DiggusMaximusClientMod.cycleShapeKey.getKeybinding().orElse(InputUtil.UNKNOWN_KEY)).setSaveConsumer(key -> shapes.cycleKey = key.getTranslationKey()).setDefaultValue(InputUtil.UNKNOWN_KEY).build());
-            shapeCat.addEntry(entryBuilder.startEnumSelector(new TranslatableText("key.diggusmaximus.config.selectedshape"), ExcavateTypes.shape.class, shapes.selectedShape).setEnumNameProvider(anEnum -> new TranslatableText("diggusmaximus.shape." + anEnum)).setSaveConsumer(val -> shapes.selectedShape = val).setDefaultValue(ExcavateTypes.shape.LAYER).build());
 
-            return builder.build();
+            ConfigSection shapeSection = new ConfigSection(configScreen, new TranslatableText("key.diggusmaximus.config.shapecat"));
+            shapeSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.enableshapes"), shapes.enableShapes, false).setSaveConsumer(val -> shapes.enableShapes = val));
+            shapeSection.addConfigItem(new BooleanItem(new TranslatableText("key.diggusmaximus.config.includedifblock"), shapes.includeDifBlocks, false).setSaveConsumer(val -> shapes.includeDifBlocks = val));
+            shapeSection.addConfigItem(new KeybindItem(new TranslatableText("key.diggusmaximus.config.shapekey"), shapes.shapeKey, "").setSaveConsumer(val -> shapes.shapeKey = val));
+            shapeSection.addConfigItem(new KeybindItem(new TranslatableText("key.diggusmaximus.config.cyclekey"), shapes.cycleKey, "").setSaveConsumer(val -> shapes.cycleKey = val));
+            shapeSection.addConfigItem(new EnumItem<>(new TranslatableText("key.diggusmaximus.config.selectedshape"), ExcavateTypes.shape.values(), shapes.selectedShape, ExcavateTypes.shape.LAYER).setSaveConsumer(val -> shapes.selectedShape = val));
+
+            return configScreen;
         };
     }
 }
